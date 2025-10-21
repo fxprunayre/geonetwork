49afe1e32f6f4da5f7552483e8cf53272f985d75
robinesavert <robine.savert@geocat.net>
2025-10-21 11:15:59 +0200

    made active filters look nicer

--- frontend/projects/library/src/lib/modules/search/search-active-filters/search-active-filters.component.ts
+++ frontend/projects/library/src/lib/modules/search/search-active-filters/search-active-filters.component.ts
@@ -2,18 +2,47 @@ import { Component, computed } from '@angular/core';
 import { SearchBase } from '../search-base/search-base';
 import { Button, ButtonIcon, ButtonLabel } from 'primeng/button';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { JsonPipe } from '@angular/common';
 import { faSolidXmark } from '@ng-icons/font-awesome/solid';
+import { TranslatePipe } from '@ngx-translate/core';
+import { AggregationTranslatePipe } from '../aggregation-translate-pipe';
+
 
 @Component({
   selector: 'app-active-filters',
-  imports: [Button, ButtonIcon, NgIcon, ButtonLabel, JsonPipe],
+  imports: [Button, ButtonIcon, NgIcon, ButtonLabel, TranslatePipe],
   viewProviders: [provideIcons({ faSolidXmark })],
   standalone: true,
+  providers: [AggregationTranslatePipe],
   templateUrl: './search-active-filters.component.html',
 })
+
 export class SearchActiveFilters extends SearchBase {
   hasActiveFilters = computed(() => {
     return Object.keys(this.search.filters()).length > 0;
   });
+
+  constructor(private aggregationTranslate: AggregationTranslatePipe) {
+    super();
+  }
+
+  getBuckets(field: string) {
+    let buckets = this.search.aggregations()[field].buckets;
+    if (Array.isArray(buckets)) {
+      return buckets;
+    }
+    return [];
+  }
+
+  getBucketLabel(groupKey: string, bucketKey: string | number): string {
+    const key = String(bucketKey);
+    const buckets = this.getBuckets(groupKey);
+    const bucket = buckets?.find(b => String(b.key) === key);
+
+    if (!bucket) return key;
+
+    return String(this.aggregationTranslate.transform(String(bucket.key), groupKey));
+  }
+
+  protected readonly Object = Object;
+  protected readonly String = String;
 }
