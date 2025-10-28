377e80bd1b66868788a618ec84a8131b31088b6d
Francois Prunayre <fx.prunayre@gmail.com>
2025-10-28 08:20:55 +0100

    Frontend / Filter / Experimenting a drawer mode.

--- frontend/projects/main/src/app/components/catalogue-component/catalogue-component.ts
+++ frontend/projects/main/src/app/components/catalogue-component/catalogue-component.ts
@@ -2,12 +2,28 @@ import { Component } from '@angular/core';
 import { SidePanel } from '../side-panel/side-panel';
 import { SearchInput, ResultViewComponent } from 'gn-library';
 import { ResultHeader } from '../result-header/result-header';
+import { Drawer } from 'primeng/drawer';
+import { Button, ButtonLabel } from 'primeng/button';
+
+export type FilterPanelLayout = 'drawer' | 'side' | 'top';
 
 @Component({
   selector: 'app-catalogue-component',
-  imports: [SidePanel, ResultViewComponent, SearchInput, ResultHeader, ResultViewComponent],
+  imports: [
+    SidePanel,
+    ResultViewComponent,
+    SearchInput,
+    ResultHeader,
+    ResultViewComponent,
+    Drawer,
+    Button,
+    ButtonLabel,
+  ],
   standalone: true,
   templateUrl: './catalogue-component.html',
   styleUrl: './catalogue-component.scss',
 })
-export class CatalogueComponent {}
+export class CatalogueComponent {
+  visible = false;
+  filterPanelMode: FilterPanelLayout = 'side';
+}
