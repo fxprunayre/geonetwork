7c6964425a7762d1525f33ebc33e72dcb59444e8
robinesavert <robine.savert@geocat.net>
2025-10-30 21:16:06 +0100

    navigation responsive

--- frontend/projects/main/src/app/components/navigation/top-navigation/top-navigation.ts
+++ frontend/projects/main/src/app/components/navigation/top-navigation/top-navigation.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { StyleClassModule } from 'primeng/styleclass';
 import { RouterLink } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { faUser } from '@ng-icons/font-awesome/regular';
+import { faSolidBars, faSolidXmark } from '@ng-icons/font-awesome/solid';
 import { FormsModule } from '@angular/forms';
 import { LanguageSwitcher } from 'gn-library';
 
@@ -12,9 +13,15 @@ import { LanguageSwitcher } from 'gn-library';
   templateUrl: './top-navigation.html',
   styleUrl: './top-navigation.scss',
   standalone: true,
-  viewProviders: [provideIcons({ faUser })],
+  viewProviders: [provideIcons({ faUser, faSolidBars, faSolidXmark })],
   imports: [ButtonModule, StyleClassModule, RouterLink, NgIcon, FormsModule, LanguageSwitcher],
 })
 export class TopNavigation {
   logo = 'images/logo.svg';
+
+  menuOpen = signal(false);
+
+  toggleMenu() {
+    this.menuOpen.update(v => !v);
+  }
 }
