53b510e8dc173908ab62757a30671476e3df2549
Francois Prunayre <fx.prunayre@gmail.com>
2025-10-31 10:03:26 +0100

    Frontend / Build with maven and pack in app.

--- src/apps/geonetwork/src/main/java/org/geonetwork/config/MvcConfiguration.java
+++ src/apps/geonetwork/src/main/java/org/geonetwork/config/MvcConfiguration.java
@@ -5,6 +5,7 @@
  */
 package org.geonetwork.config;
 
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,8 +16,17 @@ public class MvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("home");
+        //        registry.addViewController("/").setViewName("home");
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/signin").setViewName("signin");
+        List<String> jsAppList = List.of("/");
+
+        jsAppList.forEach(app -> {
+            String indexPath = "forward:" + app + "index.html";
+            registry.addViewController(app + "{path1:[a-zA-Z0-9_-]+}").setViewName(indexPath);
+            registry.addViewController(app + "{path1}/{path2:[a-zA-Z0-9_-]+}").setViewName(indexPath);
+            registry.addViewController(app + "{path1}/{path2}/{path3:[a-zA-Z0-9_-]+}")
+                    .setViewName(indexPath);
+        });
     }
 }
