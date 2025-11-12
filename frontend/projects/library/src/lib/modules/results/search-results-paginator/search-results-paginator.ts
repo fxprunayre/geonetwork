eac92c410e4ddcbda48164ddc9039a38cc6d9744
Francois Prunayre <fx.prunayre@gmail.com>
2025-11-12 14:43:54 +0100

    Frontend / Paging / Scroll to result container top.

--- frontend/projects/library/src/lib/modules/results/search-results-paginator/search-results-paginator.ts
+++ frontend/projects/library/src/lib/modules/results/search-results-paginator/search-results-paginator.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { SearchBase } from '../../search/search-base/search-base';
 import { Paginator } from 'primeng/paginator';
 import { APPLICATION_CONFIGURATION } from '../../config/config.loader';
@@ -10,14 +10,17 @@ import { APPLICATION_CONFIGURATION } from '../../config/config.loader';
   templateUrl: './search-results-paginator.html',
 })
 export class SearchResultsPaginator extends SearchBase {
+  resultsContainerRef = input<HTMLElement>();
   pageSizeOptions = inject(APPLICATION_CONFIGURATION).config?.apps.search?.hitsPerPageOptions;
 
   onPageChange(event: any) {
+    const containerRef = this.resultsContainerRef();
+    if (containerRef) {
+      containerRef.scrollIntoView({
+        behavior: 'instant',
+        block: 'start',
+      });
+    }
     this.search.setPage(event.page, event.rows);
-
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth',
-    });
   }
 }
