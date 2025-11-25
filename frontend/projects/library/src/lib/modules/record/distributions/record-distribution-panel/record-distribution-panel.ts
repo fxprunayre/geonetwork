3e902d4a77740e39dff27eb38e7e1d65088b78d5
Francois Prunayre <fx.prunayre@gmail.com>
2025-11-25 13:22:44 +0100

    Frontend / Record page / Add link to explore when any link URL is csv, parquet or json.

--- frontend/projects/library/src/lib/modules/record/distributions/record-distribution-panel/record-distribution-panel.ts
+++ frontend/projects/library/src/lib/modules/record/distributions/record-distribution-panel/record-distribution-panel.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { KeyValuePipe } from '@angular/common';
 import { Badge } from 'primeng/badge';
 import { Button } from 'primeng/button';
 import { Link } from 'gn-api-client';
 import { RecordDistributionFieldBase } from '../record-distribution-field-base/record-distribution-field-base';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-record-distribution-panel',
@@ -11,6 +12,21 @@ import { RecordDistributionFieldBase } from '../record-distribution-field-base/r
   templateUrl: './record-distribution-panel.html',
 })
 export class RecordDistributionPanel extends RecordDistributionFieldBase {
+  private router = inject(Router);
+
+  isExplorable = (link: Link) => {
+    const url = link.urlObject?.['default'];
+    if (url) {
+      return url.endsWith('.parquet') || url.endsWith('.json') || url.endsWith('.csv');
+    }
+    return false;
+  };
+  exploreData = (link: Link) => {
+    // TODO: Not sure how to link actions to routing which is app specific
+    this.router.navigate(['record', this.record().uuid, 'explore'], {
+      queryParams: { datasource: link.urlObject?.['default'] },
+    });
+  };
   addWmsLayers = (link: Link[]) => {
     // TODO
   };
