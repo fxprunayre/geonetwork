c80b3de54ecc63cee1a1c2f863bc2312a2bbf879
Francois Prunayre <fx.prunayre@gmail.com>
2025-12-10 11:21:03 +0100

    Frontend / Record page / We may want to group bboxes in one map at some point.

--- frontend/projects/library/src/lib/modules/record/record-field-coverage-spatial/record-field-coverage-spatial.ts
+++ frontend/projects/library/src/lib/modules/record/record-field-coverage-spatial/record-field-coverage-spatial.ts
@@ -88,8 +88,13 @@ export class RecordFieldCoverageSpatial extends RecordFieldBase {
     );
   });
 
+  geometryCollection = computed(() => {
+    const wktGeoms = this.geoms().map((g: any) => g.wkt);
+    return `GEOMETRYCOLLECTION(${wktGeoms.join(',')})`;
+  });
+
   overviewUrl = computed(() => {
-    return `${this.catalogueUrl}/srv/api/regions/geom.png?geomsrs=EPSG:4326&geom=${this.geoms()[0]?.wkt}`;
+    return `${this.catalogueUrl}/srv/api/regions/geom.png?geomsrs=EPSG:4326&geom=${this.geometryCollection()}`;
   });
 
   catalogueUrl: string | undefined = inject(APPLICATION_CONFIGURATION).catalogueUrl;
