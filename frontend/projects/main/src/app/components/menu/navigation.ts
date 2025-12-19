c410abbd36ceeb691c1ad8b00cd61c4d197c0fd3
Francois Prunayre <fx.prunayre@gmail.com>
2025-12-19 16:11:10 +0100

    Icons improvement & draft record page test
    * Add utility to create class name from an ng-icon (can be useful when primeng does not provide a template icon eg. in menu)
    * Record page / Quick action / Add translations
    * Record page / Quick action / Fix permalink retrieval
    * Record page / Add a draft test

--- frontend/projects/main/src/app/components/menu/navigation.ts
+++ frontend/projects/main/src/app/components/menu/navigation.ts
@@ -1,5 +1,5 @@
 import { NgTemplateOutlet } from '@angular/common';
-import { Component, computed, effect, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, ElementRef, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
@@ -8,13 +8,20 @@ import {
   faSolidArrowRightFromBracket,
   faSolidArrowRightToBracket,
   faSolidBars,
+  faSolidEllipsisVertical,
   faSolidGear,
   faSolidHouse,
   faSolidLanguage,
   faSolidMagnifyingGlass,
   faSolidPlus,
 } from '@ng-icons/font-awesome/solid';
-import { APPLICATION_CONFIGURATION, LanguageSwitcher, ThemeDesigner } from 'gn-library';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import {
+  APPLICATION_CONFIGURATION,
+  IconStyleService,
+  LanguageSwitcher,
+  ThemeDesigner,
+} from 'gn-library';
 import { MenuItem, SharedModule } from 'primeng/api';
 import { Drawer } from 'primeng/drawer';
 import { Fieldset } from 'primeng/fieldset';
@@ -22,46 +29,46 @@ import { IftaLabel } from 'primeng/iftalabel';
 import { Menu } from 'primeng/menu';
 import { TextareaModule } from 'primeng/textarea';
 import AppTheme from '../../app.theme';
-import { TranslatePipe } from '@ngx-translate/core';
+
+const ICONS = {
+  faSolidHouse,
+  faSolidBars,
+  faSolidEllipsisVertical,
+  faSolidPlus,
+  faSolidGear,
+  faSolidMagnifyingGlass,
+  faMap,
+  faCompass,
+  faSolidLanguage,
+  faSolidArrowRightFromBracket,
+  faSolidArrowRightToBracket,
+};
 
 @Component({
   selector: 'app-navigation',
   imports: [
     Menu,
     RouterLink,
-    RouterLinkActive,
     LanguageSwitcher,
     ThemeDesigner,
     NgIcon,
-    NgTemplateOutlet,
     SharedModule,
     Drawer,
     Fieldset,
     IftaLabel,
     FormsModule,
     TextareaModule,
-    TranslatePipe,
   ],
   standalone: true,
-  viewProviders: [
-    provideIcons({
-      faSolidHouse,
-      faSolidBars,
-      faSolidPlus,
-      faSolidGear,
-      faSolidMagnifyingGlass,
-      faMap,
-      faCompass,
-      faSolidLanguage,
-      faSolidArrowRightFromBracket,
-      faSolidArrowRightToBracket,
-    }),
-  ],
+  viewProviders: [provideIcons(ICONS)],
   templateUrl: './navigation.html',
 })
 export class Navigation implements OnInit {
   logo = 'images/logo.svg';
 
+  styleService = inject(IconStyleService);
+  translateService = inject(TranslateService);
+  elementRef = inject(ElementRef);
   appConfig = inject(APPLICATION_CONFIGURATION);
   appConfigJson = computed(() => JSON.stringify(this.appConfig(), null, 2));
 
@@ -69,41 +76,41 @@ export class Navigation implements OnInit {
     this.appConfig.set(JSON.parse(event));
   }
 
+  itemConfig = computed<MenuItem>(() => {
+    return {
+      iconClass: 'md:text-2xl',
+    };
+  });
+
   items = computed<MenuItem[] | undefined>(() => {
+    this.currentLang();
     return [
       {
-        icon: 'faSolidBars',
-        label: '',
-        command: () => {
-          this.toggleMenu();
-        },
-      },
-      {
-        separator: true,
-      },
-      {
-        label: 'home',
+        label: this.translateService.instant('menu.home'),
         icon: 'faCompass',
         routerLink: '/',
+        ...this.itemConfig(),
       },
       {
-        label: 'search',
+        label: this.translateService.instant('menu.search'),
         icon: 'faSolidMagnifyingGlass',
         routerLink: '/search',
+        ...this.itemConfig(),
       },
       {
-        label: 'map',
+        label: this.translateService.instant('menu.map'),
         icon: 'faMap',
         command: () => {
           window.open(`https://sextant.ifremer.fr/geonetwork/srv/fre/catalog.search#/map`, 'map');
         },
+        ...this.itemConfig(),
       },
       {
         separator: true,
         styleClass: 'mb-10',
       },
       {
-        label: 'signin',
+        label: this.translateService.instant('menu.signin'),
         visible: !this.isAuthenticated(),
         icon: 'faSolidArrowRightToBracket',
         command: () => {
@@ -111,13 +118,13 @@ export class Navigation implements OnInit {
         },
       },
       {
-        label: 'addrecord',
+        label: this.translateService.instant('menu.addrecord'),
         icon: 'faSolidPlus',
         styleClass: 'font-bold',
         visible: this.isAuthenticated(),
       },
       {
-        label: 'signout',
+        label: this.translateService.instant('menu.signout'),
         visible: this.isAuthenticated(),
         icon: 'faSolidArrowRightFromBracket',
         command: () => {
@@ -128,7 +135,7 @@ export class Navigation implements OnInit {
         separator: true,
       },
       {
-        label: 'settings',
+        label: this.translateService.instant('menu.settings'),
         icon: 'faSolidGear',
         command: () => {
           this.isConfigurationVisible.update((v) => !v);
@@ -177,11 +184,44 @@ export class Navigation implements OnInit {
 
   isAuthenticated = signal(false);
 
-  ngOnInit() {}
+  currentLang = signal(this.translateService.getCurrentLang());
+
+  ngOnInit() {
+    this.translateService.onLangChange.subscribe((event) => {
+      this.currentLang.set(event.lang);
+    });
+
+    const usedIcons = new Set<string>();
+    const collectIcons = (items: MenuItem[]) => {
+      items.forEach((item) => {
+        if (item.icon) {
+          usedIcons.add(item.icon);
+        }
+        if (item.items) {
+          collectIcons(item.items);
+        }
+      });
+    };
+
+    const menuItems = this.items();
+    if (menuItems) {
+      collectIcons(menuItems);
+    }
+
+    this.styleService.ensureIconsStyle(
+      'navigation-icon-style',
+      Array.from(usedIcons)
+        .map((icon) => ({
+          className: icon,
+          svgContent: ICONS[icon as keyof typeof ICONS],
+        }))
+        .filter((def) => def.svgContent),
+      this.elementRef.nativeElement.getRootNode(),
+    );
+  }
 
   toggleMenu() {
     this.isIconMode.update((v) => !v);
-    console.log(this.isIconMode());
   }
 
   theme = AppTheme;
