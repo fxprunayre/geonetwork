090955dfd39669333863a96202f82c7efcf6608e
Francois Prunayre <fx.prunayre@gmail.com>
2026-02-03 16:22:09 +0100

    Test / CI / Fix access to clipboard on gitlab

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -5,7 +5,13 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://0.0.0.0:4200',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'electron') {
+          launchOptions.preferences.default['profile.managed_default_content_settings.clipboard'] =
+            1;
+        }
+        return launchOptions;
+      });
     },
   },
 });
