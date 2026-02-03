fc854f9ecb9bcf8bbc6e03daa18395d83ccda4b2
Francois Prunayre <fx.prunayre@gmail.com>
2026-02-03 15:41:58 +0100

    Test / CI / Fix timeout on gitlab.

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   includeShadowDom: true,
   e2e: {
-    baseUrl: 'http://localhost:4200',
+    baseUrl: 'http://0.0.0.0:4200',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
