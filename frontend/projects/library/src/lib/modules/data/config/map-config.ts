3b8627f2875efbf632f62d907fd792d3b2362e65
Francois Prunayre <fx.prunayre@gmail.com>
2026-08-07 12:06:39 +0200

    Map / Default context with Sextant basemap - not working due to some grid error.

--- frontend/projects/library/src/lib/modules/data/config/map-config.ts
+++ frontend/projects/library/src/lib/modules/data/config/map-config.ts
@@ -1,5 +1,11 @@
-export const DEFAULT_MAP_CONTEXT = {
-  layers: [
+export const DEFAULT_MAP_VIEW = {
+  extent: [-180, -90, 180, 90],
+  maxZoom: 12,
+};
+
+export const OSM_MAP_CONTEXT = {
+  layers: [],
+  backgroundLayers: [
     {
       type: 'xyz',
       id: 'basemap-osm',
@@ -13,8 +19,32 @@ export const DEFAULT_MAP_CONTEXT = {
       },
     },
   ],
-  view: {
-    extent: [-180, -90, 180, 90],
-    maxZoom: 12,
-  },
+  view: DEFAULT_MAP_VIEW,
 };
+
+export const SEXTANT_MAP_CONTEXT = {
+  layers: [],
+  backgroundLayers: [
+    {
+      type: 'xyz',
+      id: 'basemap-osm',
+      url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+      visibility: true,
+      opacity: 1,
+      label: 'OpenStreetMap',
+      attributions: '© OpenStreetMap contributors',
+    },
+    {
+      type: 'wmts',
+      id: 'sextant',
+      url: 'https://sextant.ifremer.fr/geowebcache/service/wmts?SERVICE=wmts&amp;REQUEST=getcapabilities&amp;VERSION=1.0.0',
+      visibility: false,
+      opacity: 1,
+      name: 'sextant',
+      label: 'Sextant',
+    },
+  ],
+  view: DEFAULT_MAP_VIEW,
+};
+
+export const DEFAULT_MAP_CONTEXT = SEXTANT_MAP_CONTEXT;
